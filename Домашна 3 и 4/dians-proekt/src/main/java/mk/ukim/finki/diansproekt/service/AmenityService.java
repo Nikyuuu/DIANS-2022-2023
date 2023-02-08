259540c76869a1bec9e7bb3edacf245a09b9ba8e
sara <andrioska.sara@gmail.com>
2023-02-08 18:15:14 +0100

    Refactoring code

--- Домашна 3 и 4/dians-proekt/src/main/java/mk/ukim/finki/diansproekt/service/AmenityService.java
+++ Домашна 3 и 4/dians-proekt/src/main/java/mk/ukim/finki/diansproekt/service/AmenityService.java
@@ -6,16 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface AmenityService {
-    List<Amenity> listAll();
 
-    Optional<Amenity> findById(String id);
-
-    List<Amenity> findByType(String type);
-
-    /*List<Amenity> findByName(String name);*/
-    Optional<Amenity> findByName(String name);
-
-    /*List<Amenity> findByNameAndType(String name, String type);*/
     Optional<Amenity> findByNameAndType(String name, String type);
 
     Optional<Amenity> search(String text);
--- Домашна 3 и 4/dians-proekt/src/main/java/mk/ukim/finki/diansproekt/web/HomeController.java
+++ Домашна 3 и 4/dians-proekt/src/main/java/mk/ukim/finki/diansproekt/web/HomeController.java
@@ -28,32 +28,18 @@ public class HomeController {
                 model.addAttribute("lat", this.amenityService.findByNameAndType(bankChoice, type).get().getLatitude());
                 model.addAttribute("lon", this.amenityService.findByNameAndType(bankChoice, type).get().getLongitude());
                 model.addAttribute("type", this.amenityService.findByNameAndType(bankChoice, type).get().getType());
-
-                System.out.println("From database - type: " + this.amenityService.findByNameAndType(bankChoice, type).get().getType());
-                System.out.println("From database - name: " + this.amenityService.findByNameAndType(bankChoice, type).get().getName());
-                System.out.println("From database - latitude: " + this.amenityService.findByNameAndType(bankChoice, type).get().getLatitude());
-                System.out.println("From database - longitude: " + this.amenityService.findByNameAndType(bankChoice, type).get().getLongitude());
             } else {
                 model.addAttribute("hasError", true);
                 model.addAttribute("error", "Не постои банкомат од избраната банка во општина Карпош");
             }
-            //}
-            System.out.println("User choice - bank: " + bankChoice);
-            System.out.println("User choice - type: " + type);
         } else if (search != null && !search.equals("")) {
-            System.out.println("search string: " + search);
             model.addAttribute("chosen", this.amenityService.search(search));
             if (this.amenityService.search(search).isPresent()) {
                 model.addAttribute("bank", this.amenityService.search(search).get());
                 model.addAttribute("bankName", this.amenityService.search(search).get().getName());
                 model.addAttribute("lat", this.amenityService.search(search).get().getLatitude());
                 model.addAttribute("lon", this.amenityService.search(search).get().getLongitude());
-
-                System.out.println("From search - bank name: " + this.amenityService.search(search).get().getName());
-                System.out.println("From search - latitude: " + this.amenityService.search(search).get().getLatitude());
-                System.out.println("From search - longitude: " + this.amenityService.search(search).get().getLongitude());
-            }
-            else {
+            } else {
                 model.addAttribute("hasError", true);
                 model.addAttribute("error", "Не постои банкомат од избраната банка во општина Карпош!");
             }
