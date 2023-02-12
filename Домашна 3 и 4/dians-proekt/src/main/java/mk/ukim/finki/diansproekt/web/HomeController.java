b4c30f419d9c42b2607db17041ba2c1805d80429
sara <andrioska.sara@gmail.com>
2023-02-12 20:39:08 +0100

    minor bug fixes and code improvement

--- Домашна 3 и 4/dians-proekt/src/main/java/mk/ukim/finki/diansproekt/web/HomeController.java
+++ Домашна 3 и 4/dians-proekt/src/main/java/mk/ukim/finki/diansproekt/web/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/home")
+@RequestMapping(value = {"/", "/home"})
 public class HomeController {
     private final AmenityService amenityService;
 
@@ -21,24 +21,17 @@ public class HomeController {
                             @RequestParam(required = false) String type,
                             @RequestParam(required = false) String search,
                             Model model) {
-        if (bankChoice != null && !bankChoice.equals("") && type != null && !type.equals("")) {
+        if (bankChoice != null && !bankChoice.isEmpty() && type != null && !type.isEmpty()) {
             if (this.amenityService.findByNameAndType(bankChoice, type).isPresent()) {
                 model.addAttribute("bank", this.amenityService.findByNameAndType(bankChoice, type).get());
-                model.addAttribute("bankName", this.amenityService.findByNameAndType(bankChoice, type).get().getName());
-                model.addAttribute("lat", this.amenityService.findByNameAndType(bankChoice, type).get().getLatitude());
-                model.addAttribute("lon", this.amenityService.findByNameAndType(bankChoice, type).get().getLongitude());
-                model.addAttribute("type", this.amenityService.findByNameAndType(bankChoice, type).get().getType());
             } else {
                 model.addAttribute("hasError", true);
                 model.addAttribute("error", "Не постои банкомат од избраната банка во општина Карпош");
             }
-        } else if (search != null && !search.equals("")) {
+        } else if (search != null && !search.isEmpty()) {
             model.addAttribute("chosen", this.amenityService.search(search));
             if (this.amenityService.search(search).isPresent()) {
                 model.addAttribute("bank", this.amenityService.search(search).get());
-                model.addAttribute("bankName", this.amenityService.search(search).get().getName());
-                model.addAttribute("lat", this.amenityService.search(search).get().getLatitude());
-                model.addAttribute("lon", this.amenityService.search(search).get().getLongitude());
             } else {
                 model.addAttribute("hasError", true);
                 model.addAttribute("error", "Не постои банкомат од избраната банка во општина Карпош!");
